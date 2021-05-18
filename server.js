e8a5c4e6ab88231928aa4ec4ac70d9c0e822cf67
Pako332 <pakoa648@gmail.com>
2021-05-18 14:21:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 // app.use(cors({credentials: true, origin: `https://${process.env.HOST}`}));
 
-const whitelist = ['https://35.234.91.68:3000', 'https://localhost:3000', 'https://backend']
+const whitelist = ['https://35.234.91.68:3000', 'https://localhost:3000', 'https://backend:3000']
 let corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
