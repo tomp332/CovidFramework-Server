a9d8e9c33716af02b6eb31794fca47fd35c4fcc1
Pch12_Projects <pch12.656@gmail.com>
2021-05-22 23:19:46 +0300

    Update loginValidator.js

--- Utils/MiddleWears/loginValidator.js
+++ Utils/MiddleWears/loginValidator.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function loginValidate(req, res, next) {
     try {
+        console.log("Backend received a request for login")
         let username = req.body.username;
         let password = req.body.password;
         if (username && password) {
