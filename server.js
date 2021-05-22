e9b8cb3b5062a97cad89174113fb5e39010ba04e
Pako332 <pakoa648@gmail.com>
2021-05-22 14:49:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,21 +14,23 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use(cors({credentials: true, origin: `https://${process.env.HOST}`}));
+//app.use(cors({credentials: true, origin: `https://${process.env.HOST}`}));
+app.use(cors({origin: `https://${process.env.HOST}`}));
 
-let allowedOrigins = ['https://www.covidframework.com', 'https://35.207.83.62:3000', 'https://35.207.83.62:443', 'https://35.207.83.62','https://localhost:3000', 'https://localhost:4443', 
-'https://localhost', 'https://frontend:443', 'https://frontend','https://backend:443', 'https://backend:3000', 'https://backend'];
-app.use(cors({
-  origin: function(origin, callback){
-    console.log(origin)
-    if(!origin) return callback(null, true);    
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }    
-    return callback(null, true);
-  }
-}));
+// let allowedOrigins = ['https://www.covidframework.com', 'https://35.207.83.62:3000', 'https://35.207.83.62:443', 'https://35.207.83.62','https://localhost:3000', 'https://localhost:4443', 
+// 'https://localhost', 'https://frontend:443', 'https://frontend','https://backend:443', 'https://backend:3000', 'https://backend'];
+// app.use(cors({
+//   origin: function(origin, callback){
+//     console.log(origin)
+//     Utils.LogToFile(origin)
+//     if(!origin) return callback(null, true);    
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       var msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }    
+//     return callback(null, true);
+//   }
+//}));
 
 
 //Routes
