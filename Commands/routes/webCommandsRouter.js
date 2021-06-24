da7066a773fbdd8f4853c94d53a6c117b45e4c08
Pch12_Projects <pch12.656@gmail.com>
2021-06-24 20:40:40 +0300

    edited

--- Commands/routes/webCommandsRouter.js
+++ Commands/routes/webCommandsRouter.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-let Command = require('../../ToolRoutes/Commands/commands.model');
-let webCookieValidator = require('../../Utils/MiddleWears/webCookieValidator');
 const psCommand = require('../../ToolRoutes/Commands/pscommand.model');
 const formidable = require('express-formidable');
 const Utils = require('../../Utils/UtilFunctions/utilFunctions')
 const ClientUtils = require('../../Utils/UtilFunctions/clientUtils')
+const psResponse = require('../../ToolRoutes/Responses/psResponse.model');
 
 //Add regular command
 router.route('/add').post((req, res) => {
@@ -20,13 +19,18 @@ router.route('/ps/add').post((req, res) => {
     const clientId = req.body.client_id;
     const command = req.body.command;
     const newPsCommand = new psCommand({command_id: commandId, client_id: clientId, command: command});
-
     newPsCommand.save()
         .then(() => res.send())
         .catch(err => {
             Utils.LogToFile(`Error adding powershell command to database ${err}`);
             res.sendStatus(500);
         })
+    if(command === 'exit'){
+        psResponse.deleteMany({client_id: clientId}, function (err) {
+            if (err)
+                Utils.LogToFile(`Error removing powershell responses for ${clientId}: ${err}`)
+        })
+    }
 });
 
 router.use(formidable())
